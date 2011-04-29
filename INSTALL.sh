002c6e7fb127190b2639a803d979575798a7da64
Dan Birken <dan@thumbtack.com>
2011-04-29 15:39:42 -0700

    Add xterm configuration

--- INSTALL.sh
+++ INSTALL.sh
@@ -2,6 +2,7 @@
 
 TIMESTAMP=`date +%s`
 
+# Install vim configuration files
 mv ~/.vimrc /tmp/.vimrc-$TIMESTAMP
 cp ./vim/.vimrc ~/.vimrc
 mkdir -p ~/.vimundo
@@ -12,3 +13,7 @@ cp ./vim/.gvimrc ~/.gvimrc
 mv ~/.vim /tmp/.vim-$TIMESTAMP
 mkdir ~/.vim
 cp -r ./vim/.vim/* ~/.vim
+
+# Install xterm configuration file
+mv ~/.Xdefaults /tmp/.Xdefaults-$TIMESTAMP
+cp ./xterm/.Xdefaults ~/.Xdefaults
