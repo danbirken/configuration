fee82dc777f06a8a10398f64577f4341ea47dbc9
Dan Birken <dan@Dans-MacBook-Pro-2.local>
2015-05-03 14:23:36 -0700

    Have the client path thing find the home dir

--- bash_scripts/client_mover.py
+++ bash_scripts/client_mover.py
@@ -3,12 +3,17 @@
 import os
 import sys
 
-client_dirs = os.listdir('/home/dan/clients/')
+client_path = os.path.join(
+    os.path.expanduser('~'),
+    'clients'
+)
+
+client_dirs = os.listdir(client_path)
 
 if len(sys.argv) < 2:
     sys.exit(1)
 
 for directory_name in sorted(directory_name.lower() for directory_name in client_dirs):
     if directory_name.startswith(sys.argv[1]):
-        print 'cd /home/dan/clients/%s/' % directory_name
+        print 'cd', os.path.join(client_path, directory_name)
         sys.exit(0)
